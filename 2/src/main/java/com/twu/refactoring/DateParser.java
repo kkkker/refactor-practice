e585012bb62a3ed232d36978bfab0666587e4189
kker <907765631@qq.com>
2020-09-14 21:28:21 +0800

    [Mamingjun]: refactor test 2

--- 2/src/main/java/com/twu/refactoring/DateParser.java
+++ 2/src/main/java/com/twu/refactoring/DateParser.java
@@ -29,19 +29,29 @@ public class DateParser {
     public Date parse() {
         int year, month, date, hour, minute;
 
-        year = getTime(0, 4, "Year string is less than 4 characters", "Year is not an integer", 2000, 2012, "Year cannot be less than 2000 or more than 2012");
+        year = getTime(0, 4,
+                "Year string is less than 4 characters", "Year is not an integer",
+                2000, 2012, "Year cannot be less than 2000 or more than 2012");
 
-        month = getTime(5, 7, "Month string is less than 2 characters", "Month is not an integer", 1, 12, "Month cannot be less than 1 or more than 12");
+        month = getTime(5, 7,
+                "Month string is less than 2 characters", "Month is not an integer",
+                1, 12, "Month cannot be less than 1 or more than 12");
 
-        date = getTime(8, 10, "Date string is less than 2 characters", "Date is not an integer", 1, 31, "Date cannot be less than 1 or more than 31");
+        date = getTime(8, 10,
+                "Date string is less than 2 characters", "Date is not an integer",
+                1, 31, "Date cannot be less than 1 or more than 31");
 
         if (dateAndTimeString.substring(11, 12).equals("Z")) {
             hour = 0;
             minute = 0;
         } else {
-            hour = getTime(11, 13, "Hour string is less than 2 characters", "Hour is not an integer", 0, 23, "Hour cannot be less than 0 or more than 23");
+            hour = getTime(11, 13,
+                    "Hour string is less than 2 characters", "Hour is not an integer",
+                    0, 23, "Hour cannot be less than 0 or more than 23");
 
-            minute = getTime(14, 16, "Minute string is less than 2 characters", "Minute is not an integer", 0, 59, "Minute cannot be less than 0 or more than 59");
+            minute = getTime(14, 16,
+                    "Minute string is less than 2 characters", "Minute is not an integer",
+                    0, 59, "Minute cannot be less than 0 or more than 59");
 
         }
 

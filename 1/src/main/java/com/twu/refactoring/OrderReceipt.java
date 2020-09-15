9fb44645eb2080dd94ecaeddcdfeed107d321396
kker <907765631@qq.com>
2020-09-15 09:27:41 +0800

    [Mamingjun]: refactor test 1

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -1,12 +1,5 @@
 package com.twu.refactoring;
 
-/**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
- * 
- */
 public class OrderReceipt {
     private Order order;
 
@@ -20,18 +13,14 @@ public class OrderReceipt {
 		String headers = "======Printing Orders======\n";
 		output.append(headers);
 
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
 		addLineItem(output);
 
 		return output.toString();
 	}
 
 	private void addLineItem(StringBuilder output) {
-		// prints lineItems
 		double totSalesTx = 0d;
 		double tot = 0d;
 		for (LineItem lineItem : order.getLineItems()) {
@@ -52,10 +41,8 @@ public class OrderReceipt {
             tot += lineItem.totalAmount() + salesTax;
 		}
 
-		// prints the state tax
 		output.append("Sales Tax").append('\t').append(totSalesTx);
 
-		// print total amount
 		output.append("Total Amount").append('\t').append(tot);
 	}
 }
